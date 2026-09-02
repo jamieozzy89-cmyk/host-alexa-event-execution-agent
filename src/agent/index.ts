3037f4b8aba491b4ad5660330eeb1350a23960a4
jamieozzy89-cmyk <jamieozzy89@gmail.com>
2026-09-02 11:14:05 +0100

    Export Phase C workflow and goal orchestrator

--- src/agent/index.ts
+++ src/agent/index.ts
@@ -3,3 +3,5 @@ export * from "./interpreter.js";
 export * from "./model.js";
 export * from "./presentation.js";
 export * from "./orchestrator.js";
+export * from "./workflow.js";
+export * from "./goal-orchestrator.js";
